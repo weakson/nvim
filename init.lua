4e3c484c5c92e49b4d86f4c8e59a0d135be6380d
weakson <950823eason@gmail.com>
2026-01-13 20:25:52 +0800

    2026/1/13 20:25

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Hotfix: ft_to_lang removed in Neovim 0.10
+local lang = vim.treesitter.language
+if lang and lang.ft_to_lang == nil then
+  lang.ft_to_lang = function(ft)
+    return ft -- fallback: simply return filetype
+  end
+end
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        'nvim-telescope/telescope.nvim',
+        version = false,
         dependencies = {
             'nvim-lua/plenary.nvim',
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
